e4155e3b7a0acaa863e061fafefeb67a7e587251
8467042733 <rupeshpal25112004@gmail.com>
2023-12-31 23:48:55 +0530

    changes in code

--- src/app.js
+++ src/app.js
@@ -12,6 +12,31 @@ app.use(express.json())
 
 // GET endpoint for sending the products to client by id
 //Endpoint - /api/v1/names/:id
+app.get('/api/v1/names/:id',(req, res) => {
+    const params = req.params;
+    const id = Number(params.id);
+
+    if(isNaN(id)) {
+        return res.status(404).send({
+            status: "failed",
+            message: "Not found!"
+        });
+    }
+    const resObj = productNames.find(productName => productName.id === id);
+
+    if(!resObj) {
+        return res.status(404).send({
+            status: "failed",
+            message: "Not found!"
+        });
+    }else{
+        res.status(200).send({
+            status: 'success',
+            message: "Product name fetched successfully",
+            data: {productName:resObj}
+        });
+    }
+})
 
 
 module.exports = app;
